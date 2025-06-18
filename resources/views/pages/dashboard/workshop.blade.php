93edf40b696344d9559cad420e6220b6500700bd
fathurrahmanrifaldi <fathurrahmanrifaldi@gmail.com>
2025-06-18 21:16:27 +0700

    new kuseioner dashbord

--- resources/views/pages/dashboard/workshop.blade.php
+++ resources/views/pages/dashboard/workshop.blade.php
@@ -165,6 +165,7 @@
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
                                     </svg>
                                     <span>Batal</span>
+                                    <br>
                                 </button>
                             </div>
                         </form>
